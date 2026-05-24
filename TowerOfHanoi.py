14141df4221fad823161f102f3cef1bdcaf1f72f
BearzRcool <markos-tcspas@outlook.com>
2026-05-24 14:02:49 -0700

    Update TowerOfHanoi.py

--- TowerOfHanoi.py
+++ TowerOfHanoi.py
@@ -7,15 +7,15 @@ def toh(n,start,temp,dest):
     global total
     total+=1
     if n == 1:
-        #print(f"move {n} from {start} to {dest}")
+        print(f"move {n} from {start} to {dest}")
         return
     toh(n-1,start,dest,temp)
-    #print(f"move {n} from {start} to {dest}")
+    print(f"move {n} from {start} to {dest}")
     toh(n-1,temp,start,dest)
 
 
     
-n = 25
+n = 3
 toh(n,"a",'b','c')
 print(f"total moves = {total}")
 print(2**n - 1)
