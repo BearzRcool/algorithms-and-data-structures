ebc0e7f719d5ed28160ca3b46769f3a16362136c
BearzRcool <markos-tcspas@outlook.com>
2026-08-02 14:57:11 -0700

    tried to do pytorch
    need to download older versions using python install manager

--- DogOrCat.py
+++ DogOrCat.py
@@ -1,3 +1,10 @@
-import numpy as py
+import numpy as np
 import matplotlib.pyplot as plt
-import 
+import matplotlib
+import torch
+import torchvision
+
+print(np.__version__)
+print(matplotlib.__version__)
+print(torch.__version__)
+print(torchvision.__version__)
